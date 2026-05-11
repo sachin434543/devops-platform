e3fc113466a18753834432efde3885ef25d91468
sachin <sachin@sachins-Laptop.local>
2026-05-11 22:09:17 +0530

    Initial DevOps platform setup

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DevOps Platform Running Successfully 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'UP' });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
