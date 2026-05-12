5ea586146d40f0c0182320d711ed102200277ca1
sachin <sachin@sachins-Laptop.local>
2026-05-12 23:03:41 +0530

    Updated app message

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Platform Running Successfully 🚀');
+   res.send("CI/CD Auto Deployment Working 🚀");
 });
 
 app.get('/health', (req, res) => {
